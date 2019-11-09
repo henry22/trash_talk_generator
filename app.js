6efd773e124181ba35e91c96941d6a6a3b92e51c
henry22 <henry811010@gmail.com>
2019-11-09 23:34:55 +0800

    Add handlebars helper

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const generateTrashTalk = require('./generate_trashtalk')
 
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+  helpers: {
+    is: function(arg1, arg2, options) {
+      if (arg1 === arg2) {
+        return options.fn(this)
+      } else {
+        return options.inverse(this)
+      }
+    }
+  }
+}))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,7 +27,8 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const occupation = req.body.occupation
-  res.render('index', { trashTalk: generateTrashTalk(occupation)})
+
+  res.render('index', { trashTalk: generateTrashTalk(occupation), occupation: occupation})
 })
 
 app.listen(port, () => console.log(`The server is listening on http://localhost:${port}`))
