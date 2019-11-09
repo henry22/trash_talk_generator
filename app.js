2d17e881f0fdd247275806e760b4f9bd61b5f2a3
henry22 <henry811010@gmail.com>
2019-11-09 21:59:33 +0800

    Install body-parser

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req body', req.body)
   res.render('index')
 })
 
